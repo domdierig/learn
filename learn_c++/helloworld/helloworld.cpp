73e6efa14c9103b62ea9d1facaea0294b28fb6c1
Dominic <dom.pilipu@outlook.de>
2020-01-08 14:30:42 +0100

    start new years resolution

---
+++ learn_c++/helloworld/helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+
+// ctrl+shift+b to build
+// .\helloworld.exe in terminal to run
---
+++ learn_c++/readme.txt
@@ -0,0 +1,6 @@
+new year resolution
+learn c++
+
+https://www.learn-cpp.org/de/Hello%2C_World%21
+https://code.visualstudio.com/docs/cpp/config-mingw
+http://www.cplusplus.com/doc/tutorial/
